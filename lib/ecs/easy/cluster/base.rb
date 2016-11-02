1c7562561a202e64e8d2f0df3f4ba94f8f0e3779
metheglin <pigmybank@gmail.com>
2016-11-02 22:35:52 +0900

    Implemented and replaced the up! and destroy! method with CloudFormation direct api conversation.

--- lib/ecs/easy/cluster/base.rb
+++ lib/ecs/easy/cluster/base.rb
@@ -1,31 +1,42 @@
 module Ecs::Easy::Cluster
   class Base
 
-    attr_reader :name, :configure, :client
+    attr_reader :name, :configure, :ecs_client, :stack_name, :cfn_client
     attr_accessor :instance, :min_instances, :max_instances
 
     def initialize( name, configure )
       @name = name
       @configure = configure
-      @client = Aws::ECS::Client.new(
-        region: configure.region
+      @ecs_client = Aws::ECS::Client.new(
+        region: configure.region,
+        credentials: configure.credentials
+      )
+      @stack_name = configure.cfn_stack_name_prefix + name
+      @cfn_client = Aws::CloudFormation::Client.new(
+        region: configure.region,
+        credentials: configure.credentials
       )
       yield( self ) if block_given?
     end
 
     # Check if the cluster exists
+    # A cluster existence should be decided as cloudformation stack existence
     def exists?
-      cluster_names = client.list_clusters.cluster_arns.map do |arn|
-        arn.split("/").last
-      end
-      cluster_names.include?( name )
+      res = cfn_client.describe_stacks( stack_name: stack_name )
+      return res.stacks.length > 0
+    rescue => e
+      return false
+      # cluster_names = ecs_client.list_clusters.cluster_arns.map do |arn|
+      #   arn.split("/").last
+      # end
+      # cluster_names.include?( name )
     end
 
     # Check if all the container instances on the cluster are ready
     def ready?
       return false unless exists?
-      instance_arns = client.list_container_instances(cluster: name).container_instance_arns
-      instances = client.describe_container_instances(
+      instance_arns = ecs_client.list_container_instances(cluster: name).container_instance_arns
+      instances = ecs_client.describe_container_instances(
         cluster: name,
         container_instances: instance_arns
       ).container_instances
@@ -34,12 +45,12 @@ module Ecs::Easy::Cluster
 
     # Check if the task exists on the cluster
     def task_exists?( task )
-      task_arns = client.list_tasks(cluster: name).task_arns
+      task_arns = ecs_client.list_tasks(cluster: name).task_arns
       task_arns.include?( task )
     end
 
     def num_instances
-      client.list_container_instances(cluster: name).container_instance_arns.length
+      ecs_client.list_container_instances(cluster: name).container_instance_arns.length
     end
 
     # Wait until all the container instances ready
@@ -52,41 +63,95 @@ module Ecs::Easy::Cluster
 
     def wait_until_task_running( arn )
       raise "Task Not Found. Possibly the task is already stopped." unless task_exists?( arn )
-      client.wait_until(:tasks_running, tasks: [arn])
+      ecs_client.wait_until(:tasks_running, tasks: [arn])
     end
 
     def run_task!( task_definition, overrides={} )
-      res = client.run_task(
+      res = ecs_client.run_task(
         cluster: name,
         task_definition: task_definition,
         overrides: overrides
       )
     end
 
+    # TODO: Support the case: min_instances > 2
     def up!
       unless exists?
-        cmd = <<-EOH
-          ecs-cli up \
-            --keypair #{instance.keypair} \
-            --capability-iam \
-            --size #{min_instances} \
-            --instance-type #{instance.type} \
-            --azs #{instance.azs} \
-            --subnets #{instance.subnets} \
-            --vpc #{instance.vpc} \
-            --security-group #{instance.security_group} \
-            --image-id #{instance.image_id}
-        EOH
-        puts cmd
+        ecs_client.create_cluster( cluster_name: name )
+        template_path = File.expand_path("../config/cloudFormation_template.json", __FILE__)
+        template_body = File.read( template_path )
+        cfn_client.create_stack(
+          stack_name: stack_name,
+          template_body: template_body,
+          parameters: [
+            {
+              parameter_key: "EcsAmiId",
+              parameter_value: instance.image_id,
+            },
+            {
+              parameter_key: "EcsInstanceType",
+              parameter_value: instance.type,
+            },
+            {
+              parameter_key: "KeyName",
+              parameter_value: instance.keypair,
+            },
+            {
+              parameter_key: "VpcId",
+              parameter_value: instance.vpc,
+            },
+            {
+              parameter_key: "SubnetIds",
+              parameter_value: instance.subnets,
+            },
+            {
+              parameter_key: "SecurityGroup",
+              parameter_value: instance.security_group,
+            },
+            {
+              parameter_key: "EcsCluster",
+              parameter_value: name,
+            },
+          ],
+          capabilities: ["CAPABILITY_IAM"],
+          on_failure: "DELETE",
+        )
+        cfn_client.wait_until(
+          :stack_create_complete, 
+          stack_name: stack_name
+        )
+        
+        # This depends on the ecs-cli command
+        # cmd = <<-EOH
+        #   ecs-cli up \
+        #     --keypair #{instance.keypair} \
+        #     --capability-iam \
+        #     --size #{min_instances} \
+        #     --instance-type #{instance.type} \
+        #     --azs #{instance.azs} \
+        #     --subnets #{instance.subnets} \
+        #     --vpc #{instance.vpc} \
+        #     --security-group #{instance.security_group} \
+        #     --image-id #{instance.image_id}
+        # EOH
+        # puts cmd
 
-        IO.popen( cmd, "r" ) do |pipe|
-          while line = pipe.gets
-            puts line
-          end
-        end
+        # IO.popen( cmd, "r" ) do |pipe|
+        #   while line = pipe.gets
+        #     puts line
+        #   end
+        # end
       end
 
       return exists?
+    end
+
+    def destroy!
+      cfn_client.delete_stack(stack_name: stack_name)
+      cfn_client.wait_until(
+        :stack_delete_complete, 
+        stack_name: stack_name
+      )
     end
 
     def scale!
--- lib/ecs/easy/configure.rb
+++ lib/ecs/easy/configure.rb
@@ -11,10 +11,21 @@ module Ecs
         :cfn_stack_name_prefix
 
       def initialize **params
+        @compose_project_name_prefix  = ""
+        @compose_service_name_prefix  = "ecscompose-service-"
+        @cfn_stack_name_prefix        = "amazon-ecs-cli-setup-"
+
         params.each do |k,v|
           self.send("#{k}=", v) if self.methods.include?(k)
         end
         yield( self ) if block_given?
+      end
+
+      def credentials
+        @credentials ||= profile ?
+          Aws::SharedCredentials.new( profile_name: profile ) :
+          Aws::Credentials.new( access_key, secret_key )
+        @credentials
       end
 
     end
